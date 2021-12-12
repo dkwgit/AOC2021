c41e654691ed9879055ca9d34883b6e095b27a60
David Wright <720482+dkwgit@users.noreply.github.com>
2021-12-12 16:18:36 -0600

    Made ConnectedCaves into a property for consistency.

--- AOC2021/Days/Day12.cs
+++ AOC2021/Days/Day12.cs
@@ -13,12 +13,12 @@ namespace AOC2021.Days
     {
         private readonly DataStore datastore;
 
-        private readonly Dictionary<string, List<string>> connectedCaves = new();
-
         public Day12(DataStore datastore)
         {
             this.datastore = datastore;
         }
+
+        private Dictionary<string, List<string>> ConnectedCaves { get; } = new();
 
         private List<List<string>> AllPaths { get; } = new();
 
@@ -34,7 +34,7 @@ namespace AOC2021.Days
             this.PrepData();
 
             GraphNode<string> caveStart = new(null, "start");
-            caveStart.BuildGraph(new List<string>(), this.connectedCaves, this.AllPaths, "end", (string s) => char.IsLower(s[0]), string.Empty);
+            caveStart.BuildGraph(new List<string>(), this.ConnectedCaves, this.AllPaths, "end", (string s) => char.IsLower(s[0]), string.Empty);
 
             long result = this.AllPaths.Count;
             return result;
@@ -48,7 +48,7 @@ namespace AOC2021.Days
             foreach (var smallCaveWithTwoVisits in this.SmallCaves.Keys)
             {
                 GraphNode<string> caveStart = new(null, "start");
-                caveStart.BuildGraph(new List<string>(), this.connectedCaves, this.AllPaths, "end", (string s) => char.IsLower(s[0]), smallCaveWithTwoVisits);
+                caveStart.BuildGraph(new List<string>(), this.ConnectedCaves, this.AllPaths, "end", (string s) => char.IsLower(s[0]), smallCaveWithTwoVisits);
             }
 
             // Dedupe the paths before counting
@@ -82,26 +82,26 @@ namespace AOC2021.Days
                     caves = temp;
                 }
 
-                if (!this.connectedCaves.ContainsKey(caves[0]))
+                if (!this.ConnectedCaves.ContainsKey(caves[0]))
                 {
-                    this.connectedCaves[caves[0]] = new List<string>();
+                    this.ConnectedCaves[caves[0]] = new List<string>();
                 }
 
-                if (!this.connectedCaves[caves[0]].Contains(caves[1]))
+                if (!this.ConnectedCaves[caves[0]].Contains(caves[1]))
                 {
-                    this.connectedCaves[caves[0]].Add(caves[1]);
+                    this.ConnectedCaves[caves[0]].Add(caves[1]);
                 }
 
                 if (produceBothDirections)
                 {
-                    if (!this.connectedCaves.ContainsKey(caves[1]))
+                    if (!this.ConnectedCaves.ContainsKey(caves[1]))
                     {
-                        this.connectedCaves[caves[1]] = new List<string>();
+                        this.ConnectedCaves[caves[1]] = new List<string>();
                     }
 
-                    if (!this.connectedCaves[caves[1]].Contains(caves[0]))
+                    if (!this.ConnectedCaves[caves[1]].Contains(caves[0]))
                     {
-                        this.connectedCaves[caves[1]].Add(caves[0]);
+                        this.ConnectedCaves[caves[1]].Add(caves[0]);
                     }
                 }
 
