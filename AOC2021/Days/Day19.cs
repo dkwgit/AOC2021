2cd5985e8b5b12519085b90e60fc1e5b522748bb
David Wright <720482+dkwgit@users.noreply.github.com>
2021-12-30 22:57:59 -0600

    Reading input ffor Day 19

--- AOC2021/Days/Day19.cs
+++ AOC2021/Days/Day19.cs
@@ -6,25 +6,45 @@
 
 namespace AOC2021.Days
 {
+    using System.Text.RegularExpressions;
     using AOC2021.Data;
+    using AOC2021.Models;
+    using MathNet.Spatial.Euclidean;
 
     public class Day19 : BaseDay, IDay
     {
         private readonly DataStore datastore;
+
+        private List<Scanner>? scanners;
 
         public Day19(DataStore datastore)
         {
             this.datastore = datastore;
         }
 
+        internal List<Scanner> Scanners
+        {
+            get
+            {
+                if (this.scanners == null)
+                {
+                    throw new InvalidOperationException("Access to Scanners property before initialized.");
+                }
+
+                return this.scanners;
+            }
+        }
+
         public override string GetDescription()
         {
-            return "";
+            return "Scanner/beacon mapping";
         }
 
         public override string Result1()
         {
             this.PrepData();
+
+            var intersections = this.Scanners.Skip(1).Select(s => s.DistancesToPoints.Keys.Where(k => s.DistancesToPoints[k].Count == 1).Intersect(this.Scanners[0].DistancesToPoints.Keys.Where(k => this.Scanners[0].DistancesToPoints[k].Count == 1)).ToList()).ToList();
 
             long result = 0;
             return result.ToString();
@@ -39,6 +59,45 @@ namespace AOC2021.Days
         internal void PrepData()
         {
             string[] lines = this.datastore.GetRawData(this.GetName());
+
+            string state = "initial";
+            int scannerId = -1;
+            List<Point3D> points = new();
+            List<Scanner> scanners = new();
+            foreach (string line in lines)
+            {
+                if (line == string.Empty)
+                {
+                    state = "initial";
+                    scanners.Add(new Scanner(scannerId, points.ToArray()));
+                    scannerId = -1;
+                    points.Clear();
+                }
+
+                if (state == "points")
+                {
+                    string[] strings = line.Split(",");
+                    double[] coordinates = strings.Select(s => double.Parse(s)).ToArray();
+                    Point3D point = new(coordinates[0], coordinates[1], coordinates[2]);
+                    points.Add(point);
+                }
+
+                if (state == "initial" && line.Contains("scanner"))
+                {
+                    string pattern = @"(\d+)";
+                    Match m = Regex.Match(line, pattern);
+                    if (m.Success)
+                    {
+                        scannerId = int.Parse(m.Groups[1].Value);
+                    }
+
+                    state = "points";
+                }
+            }
+
+            scanners.Add(new Scanner(scannerId, points.ToArray()));
+
+            this.scanners = scanners;
         }
     }
 }
---
+++ AOC2021/Models/Scanner.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Scanner.cs" company="David Wright">
+// Copyright (c) David Wright. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AOC2021.Models
+{
+    using MathNet.Spatial.Euclidean;
+
+    internal class Scanner
+    {
+        internal Scanner(int id, Point3D[] points)
+        {
+            this.Id = id;
+            this.Points = points;
+
+            List<Point3D> pointList = new(points);
+            Point3D current = pointList[0];
+            pointList.RemoveAt(0);
+            while (pointList.Count > 0)
+            {
+                foreach (var p in pointList)
+                {
+                    Line3D line = new(current, p);
+                    double distance = line.Length;
+
+                    if (!this.DistancesToPoints.ContainsKey(distance))
+                    {
+                        List<(Point3D Start, Point3D End, Line3D Line)> list = new();
+                        this.DistancesToPoints[distance] = list;
+                    }
+
+                    this.DistancesToPoints[distance].Add((current, p, line));
+                }
+
+                current = pointList[0];
+                pointList.RemoveAt(0);
+            }
+        }
+
+        internal int Id { get; }
+
+        internal Point3D[] Points { get; }
+
+        internal Dictionary<double, List<(Point3D Start, Point3D End, Line3D Line)>> DistancesToPoints { get; } = new();
+    }
+}
