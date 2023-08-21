d79e198fdc66bfe631756c5ea501da795305c6f6
David Wright <720482+dkwgit@users.noreply.github.com>
2023-08-21 14:18:10 -0500

    Catchup commit

--- AOC2021/Days/Day22.cs
+++ AOC2021/Days/Day22.cs
@@ -7,10 +7,13 @@
 namespace AOC2021.Days
 {
     using AOC2021.Data;
+    using AOC2021.Models;
 
     internal class Day22 : BaseDay, IDay
     {
         private readonly DataStore datastore;
+
+        private readonly List<Cuboid> cuboids = new();
 
         public Day22(DataStore datastore)
         {
@@ -24,19 +27,141 @@ namespace AOC2021.Days
 
         public override string Result1()
         {
-            long result = 0;
+            this.PrepData();
+
+            HashSet<(int X, int Y, int Z)> onLocations = new();
+            ProcessCuboids(this.cuboids.Where(c => c.InInit), onLocations);
+
+            long result = onLocations.Count;
             return result.ToString();
         }
 
         public override string Result2()
         {
-            long result = 0;
+            List<Cuboid> masterList = new();
+            Cuboid.PublishCreate = (Cuboid c) => { masterList.Add(c); };
+            this.cuboids.Clear();
+            this.PrepData();
+            masterList = masterList.Where(c => c.InInit).ToList();
+
+            Cuboid startCuboid = this.cuboids.First();
+
+            List<Cuboid> processedCuboids = new() { startCuboid };
+            List<Cuboid> unprocessedCuboids = this.cuboids.Skip(1).Take(19).ToList();
+
+            while (unprocessedCuboids.Count > 0)
+            {
+                Cuboid unprocessed = unprocessedCuboids.First();
+
+                var discard = processedCuboids.Select(c =>
+                {
+                    c.Cull(unprocessed);
+
+                    return c;
+                }).Count();
+
+                processedCuboids = processedCuboids.Where(p => !p.Deleted).ToList();
+
+                foreach (Cuboid p in processedCuboids)
+                {
+                    p.Intersect(unprocessed);
+                }
+
+                processedCuboids.Add(unprocessed);
+                unprocessedCuboids = unprocessedCuboids.Skip(1).ToList();
+            }
+
+            long result = processedCuboids.Where(p => p.On && !p.Deleted).Sum(p => p.Count);
             return result.ToString();
+        }
+
+        internal static void ProcessCuboids(IEnumerable<Cuboid> cuboids, HashSet<(int X, int Y, int Z)> onLocations)
+        {
+            int index = 0;
+            foreach (Cuboid cuboid in cuboids)
+            {
+                var process = (
+                    from x in Enumerable.Range(cuboid.MinX, cuboid.MaxX - cuboid.MinX + 1)
+                    from y in Enumerable.Range(cuboid.MinY, cuboid.MaxY - cuboid.MinY + 1)
+                    from z in Enumerable.Range(cuboid.MinZ, cuboid.MaxZ - cuboid.MinZ + 1)
+                    select (x, y, z)).Select(triplet =>
+                    {
+                        if (cuboid.On)
+                        {
+                            if (!onLocations.Contains(triplet))
+                            {
+                                onLocations.Add(triplet);
+                            }
+                        }
+                        else
+                        {
+                            if (onLocations.Contains(triplet))
+                            {
+                                onLocations.Remove(triplet);
+                            }
+                        }
+
+                        return 1;
+                    });
+
+                long discard = process.Count();
+                index++;
+            }
         }
 
         internal void PrepData()
         {
-            string[] data = this.datastore.GetRawData(this.GetName());
+            string[] lines = this.datastore.GetRawData(this.GetName());
+            foreach (string s in lines)
+            {
+                string line = s;
+
+                bool on = false;
+                int minX = -1;
+                int maxX = -1;
+                int minY = -1;
+                int maxY = -1;
+                int minZ = -1;
+                int maxZ = -1;
+
+                if (line.Contains("on "))
+                {
+                    on = true;
+                    line = line.Replace("on ", string.Empty);
+                }
+                else if (line.Contains("off "))
+                {
+                    on = false;
+                    line = line.Replace("off ", string.Empty);
+                }
+
+                line = line.Replace("x=", string.Empty);
+                line = line.Replace("y=", string.Empty);
+                line = line.Replace("z=", string.Empty);
+
+                string[] parts = line.Split(',');
+
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    string[] minMax = parts[i].Split("..");
+                    if (i == 0)
+                    {
+                        (minX, maxX) = (int.Parse(minMax[0]), int.Parse(minMax[1]));
+                    }
+                    else if (i == 1)
+                    {
+                        (minY, maxY) = (int.Parse(minMax[0]), int.Parse(minMax[1]));
+                    }
+                    else
+                    {
+                        (minZ, maxZ) = (int.Parse(minMax[0]), int.Parse(minMax[1]));
+                    }
+                }
+
+                List<int> allCoords = new() { minX, maxX, minY, maxY, minZ, maxZ };
+                bool inInit = allCoords.All(x => Math.Abs(x) <= 50);
+                this.cuboids.Add(Cuboid.GetCuboid(on, minX, maxX, minY, maxY, minZ, maxZ, inInit, "primary"));
+            }
         }
     }
 }
---
+++ AOC2021/Models/Cuboid.cs
@@ -0,0 +1,153 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Cuboid.cs" company="David Wright">
+// Copyright (c) David Wright. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AOC2021.Models
+{
+    using FluentAssertions;
+
+    internal record class Cuboid(int Id, bool On, int MinX, int MaxX, int MinY, int MaxY, int MinZ, int MaxZ, bool InInit, string Type)
+    {
+        public static Action<Cuboid>? PublishCreate = null;
+
+        private static int NextId = 1;
+
+        private long? count = null;
+
+        internal long Count
+        {
+            get
+            {
+                if (!this.count.HasValue)
+                {
+                    this.count = ((long)(Math.Abs(this.MaxX - this.MinX) + 1)) * ((long)(Math.Abs(this.MaxY - this.MinY) + 1)) * ((long)(Math.Abs(this.MaxZ - this.MinZ) + 1));
+                }
+
+                return this.count.Value;
+            }
+
+            set
+            {
+                this.count = value;
+            }
+        }
+
+        internal Cuboid? Parent1 { get; set; } = null;
+
+        internal Cuboid? Parent2 { get; set; } = null;
+
+        internal bool Deleted { get; set; } = false;
+
+        internal List<Cuboid> Children { get; } = new();
+
+        internal static Cuboid GetCuboid(bool on, int minX, int maxX, int minY, int maxY, int minZ, int maxZ, bool inInit, string type = "primary")
+        {
+            int id = Cuboid.NextId++;
+            Cuboid newCuboid = new(id, on, minX, maxX, minY, maxY, minZ, maxZ, inInit, type);
+
+            if (PublishCreate is not null)
+            {
+                PublishCreate(newCuboid);
+            }
+
+            return newCuboid;
+        }
+
+        internal void Intersect(Cuboid other)
+        {
+            int? minX = null;
+            int? maxX = null;
+            int? minY = null;
+            int? maxY = null;
+            int? minZ = null;
+            int? maxZ = null;
+
+            if ((other.MinX >= this.MinX && other.MinX <= this.MaxX) || (other.MaxX >= this.MinX && other.MaxX <= this.MaxX))
+            {
+                minX = (other.MinX >= this.MinX && other.MinX <= this.MaxX) ? other.MinX : this.MinX;
+                maxX = (other.MaxX >= this.MinX && other.MaxX <= this.MaxX) ? other.MaxX : this.MaxX;
+            }
+
+            if ((other.MinY >= this.MinY && other.MinY <= this.MaxY) || (other.MaxY >= this.MinY && other.MaxY <= this.MaxY))
+            {
+                minY = (other.MinY >= this.MinY && other.MinY <= this.MaxY) ? other.MinY : this.MinY;
+                maxY = (other.MaxY >= this.MinY && other.MaxY <= this.MaxY) ? other.MaxY : this.MaxY;
+            }
+
+            if ((other.MinZ >= this.MinZ && other.MinZ <= this.MaxZ) || (other.MaxZ >= this.MinZ && other.MaxZ <= this.MaxZ))
+            {
+                minZ = (other.MinZ >= this.MinZ && other.MinZ <= this.MaxZ) ? other.MinZ : this.MinZ;
+                maxZ = (other.MaxZ >= this.MinZ && other.MaxZ <= this.MaxZ) ? other.MaxZ : this.MaxZ;
+            }
+
+            List<int?> allCoords = new() { minX, maxX, minY, maxY, minZ, maxZ };
+            bool allCoordsSet = allCoords.All(c => c.HasValue);
+
+            if (allCoordsSet)
+            {
+                bool inInit = allCoords.All(x => Math.Abs(x!.Value) <= 50);
+                Cuboid intersection = Cuboid.GetCuboid(other.On, minX!.Value, maxX!.Value, minY!.Value, maxY!.Value, minZ!.Value, maxZ!.Value, inInit, "intersection");
+
+                intersection.Parent1 = this;
+                intersection.Parent2 = other;
+
+                this.Children.Add(intersection);
+                other.Children.Add(intersection);
+
+                this.Count -= intersection.Count;
+                other.Count -= intersection.Count;
+            }
+        }
+
+        internal bool Contains(Cuboid other)
+        {
+            if (this.MinX <= other.MinX && this.MaxX >= other.MaxX && this.MinY <= other.MinY && this.MaxY >= other.MaxY && this.MinZ <= other.MinZ && this.MaxZ >= other.MaxZ)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        internal void Cull(Cuboid other)
+        {
+            foreach (Cuboid child in this.Children)
+            {
+                child.Cull(other);
+            }
+
+            if (other.Contains(this))
+            {
+                this.Delete();
+            }
+
+            if (this.Count <= 0)
+            {
+                this.Delete();
+            }
+        }
+
+        internal void Delete()
+        {
+            foreach (Cuboid child in this.Children)
+            {
+                child.Delete();
+            }
+
+            this.Children.Clear();
+
+            this.Deleted = true;
+            if (this.Parent1 != null)
+            {
+                this.Parent1 = null;
+            }
+
+            if (this.Parent2 != null)
+            {
+                this.Parent1 = null;
+            }
+        }
+    }
+}
